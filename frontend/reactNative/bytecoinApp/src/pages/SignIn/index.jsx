e2d5deb5cc5e7797cb8b7d55bc51a3711881a98e
byankatomaz <byankatomaz@gmail.com>
2023-12-01 00:13:41 -0300

    :tada:

---
+++ frontend/reactNative/bytecoinApp/src/pages/SignIn/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>Tela Login</Text>
+    </View>
+  )
+}
---
+++ frontend/reactNative/bytecoinApp/src/pages/Welcome/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import styles from './styles'
+
+
+export default function Welcome() {
+    return (
+      <View style={styles.container}>
+        <Text> TELA BEM VINDO </Text>
+      </View>
+    )
+}
